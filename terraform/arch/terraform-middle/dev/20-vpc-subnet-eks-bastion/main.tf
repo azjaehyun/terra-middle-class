4ba1f1c18da9fa068939dce698fd802aca17252b
azjaehyun <azjaehyun@gmail.com>
2023-01-25 13:39:55 +0900

    dd

--- terraform/arch/terraform-middle/dev/20-vpc-subnet-eks-bastion/main.tf
+++ terraform/arch/terraform-middle/dev/20-vpc-subnet-eks-bastion/main.tf
@@ -4,7 +4,7 @@
 ## data source : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/key_pair
 module "aws_key_pair" {
   source = "../../../../modules/aws/keypair"
-  keypair_name   = "${var.keypair_name}"
+  keypair_name   = var.keypair_name
   tag_name = merge(local.tags, {Name = format("%s-key", local.name_prefix)})
 }
 
